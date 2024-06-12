48f81c17d9494ea707eb0100b5af908b0ff4919f
SayaaBrink <sayabrink@gmail.com>
2024-06-12 09:34:03 -0600

    Moved am_pm input inside convert_to_military

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 standard_time = input('Enter a time in standard time: ')
-am_pm = input('AM or PM?')
 
 
-def convert_to_military (time, time_of_day) :
+def convert_to_military (time):
+    am_pm = input('AM or PM?')
     if time == "1200":
-        if time_of_day == 'AM':
-            return "0000"
+        if am_pm == 'AM':
+            return "0000" 
         else:
-            return time
+            return time 
         
-    if time_of_day == 'PM':
+    if am_pm == 'PM':
         new_time = int(time) + 1200
-        return str(new_time)
+        return str(new_time) 
     else :
-        return time
+        return time 
    
-# print(standard_time + ' in military time is:\n' + convert_to_military(standard_time, am_pm))
-print(f'{standard_time} in military time is:\n {convert_to_military(standard_time, am_pm)}')
+
+print(f'{standard_time} in military time is:\n {convert_to_military(standard_time)}')
+
+
+
+
